384fff1868be9e4f84f0490fdc692907515d37e4
Ignacio-PerezAllub <perezallub.ignaciomartin@gmail.com>
2022-11-12 00:09:26 +0100

    update

--- app/src/main/java/com/example/pochedex/MainActivity.java
+++ app/src/main/java/com/example/pochedex/MainActivity.java
@@ -35,7 +35,6 @@ public class MainActivity extends AppCompatActivity implements RecyclerViewInter
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         RecyclerView recyclerView = findViewById(R.id.pocheRecyclerView);
         getMyPochemon(MainActivity.this);
         poche_RecyclerViewAdapter adapter = new poche_RecyclerViewAdapter(
